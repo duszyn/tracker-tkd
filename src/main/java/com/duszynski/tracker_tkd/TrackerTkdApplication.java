25eaf94ac854691ee91a6f58655bbf4c384bd675
duszyn <kubon20041@gmail.com>
2026-09-19 15:58:17 +0200

    Add Spring Boot initial project
    REST API for tracking student attendance and engagement during practice sessions

---
+++ src/main/java/com/duszynski/tracker_tkd/TrackerTkdApplication.java
@@ -0,0 +1,13 @@
+package com.duszynski.tracker_tkd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackerTkdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackerTkdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duszynski/tracker_tkd/TrackerTkdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duszynski.tracker_tkd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackerTkdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
